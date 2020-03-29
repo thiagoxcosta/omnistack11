7d7f3d1b606c30041f36bba3a73ddeb03a17315b
Thiago Costa <thiago.x.costa@gmail.com>
2020-03-29 16:14:25 -0300

    Mobile da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -6,7 +6,7 @@ module.exports = {
 
         const [count] = await connection('incidents').count();
 
-        const incidents = await connection('incidents').join('ongs', 'ongs_id', '=', 'incidents.ong_id')
+        const incidents = await connection('incidents').join('ongs', 'ongs.id', '=', 'incidents.ong_id')
                                                        .limit(5)
                                                        .offset((page - 1) * 5) 
                                                        .select([
@@ -18,7 +18,7 @@ module.exports = {
                                                            'ongs.uf'
                                                         ]);
 
-        response.header('X-Total-Count', count['count*']);
+        response.header('X-Total-Count', count['count(*)']);
         return response.json(incidents);
     },
 
